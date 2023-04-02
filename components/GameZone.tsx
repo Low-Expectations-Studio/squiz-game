53153d06f243539d3566b12fd62ac89d814ee96e
Nadir Kadem <nadir.kadem@gmail.com>
2023-04-02 21:02:54 +0200

    [ui] added new font%

--- components/GameZone.tsx
+++ components/GameZone.tsx
@@ -3,7 +3,7 @@ import { Timer } from './Timer';
 export const GameZone = ({ className }: { className: string }) => {
   return (
     <div
-      className={`${className} rounded-xl border-2 border-gray-600 border-opacity-30 p-4 backdrop-blur-md`}
+      className={`${className} rounded-xl border-2 border-gray-600 border-opacity-30 p-4 backdrop-blur-lg`}
     >
       <Timer duration={100} />
     </div>
