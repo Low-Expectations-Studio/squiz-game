1ab0573abddc232a386cc7c18a87b3e09e43e07c
Nadir Kadem <nadir.kadem@gmail.com>
2023-04-02 22:46:32 +0200

    [ui] add zoom for image%

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,11 +1,19 @@
+'use client';
+
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import React, { useEffect } from 'react';
+
 export const Question = () => {
   return (
     <div className="flex w-full gap-6">
       <textarea className="flex-1 rounded-lg p-6"></textarea>
-      <img
-        src="https://www.enigme-facile.fr/wp-content/uploads/2020/11/25-films-enigme.jpg"
-        className="max-w-[50%] rounded-lg"
-      />
+      <div className="max-w-[50%] rounded-lg">
+        <TransformWrapper>
+          <TransformComponent>
+            <img src="https://www.enigme-facile.fr/wp-content/uploads/2020/11/25-films-enigme.jpg" />
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
     </div>
   );
 };
