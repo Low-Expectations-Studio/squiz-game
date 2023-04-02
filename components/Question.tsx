9af29f3c0751f8046a1979fc09d7d46ba19928ea
Nadir Kadem <nadir.kadem@gmail.com>
2023-04-02 21:48:21 +0200

    [ui] better image sizing%

--- components/Question.tsx
+++ components/Question.tsx
@@ -4,7 +4,7 @@ export const Question = () => {
       <textarea className="flex-1 rounded-lg p-6"></textarea>
       <img
         src="https://www.enigme-facile.fr/wp-content/uploads/2020/11/25-films-enigme.jpg"
-        className="rounded-lg"
+        className="max-w-[50%] rounded-lg"
       />
     </div>
   );
