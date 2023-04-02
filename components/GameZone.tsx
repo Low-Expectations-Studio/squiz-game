55fabf460a0e282217d76b0994a099ca803e7ae0
Nadir Kadem <nadir.kadem@gmail.com>
2023-04-02 21:41:33 +0200

    [ui] add Question%

--- components/GameZone.tsx
+++ components/GameZone.tsx
@@ -1,11 +1,13 @@
+import { Question } from './Question';
 import { Timer } from './Timer';
 
 export const GameZone = ({ className }: { className: string }) => {
   return (
     <div
-      className={`${className} rounded-xl border-2 border-gray-600 border-opacity-30 p-4 backdrop-blur-lg`}
+      className={`flex flex-col gap-6 ${className} rounded-xl border-2 border-gray-600 border-opacity-30 p-6 backdrop-blur-lg`}
     >
       <Timer duration={100} />
+      <Question />
     </div>
   );
 };
---
+++ components/Question.tsx
@@ -0,0 +1,11 @@
+export const Question = () => {
+  return (
+    <div className="flex w-full gap-6">
+      <textarea className="flex-1 rounded-lg p-6"></textarea>
+      <img
+        src="https://www.enigme-facile.fr/wp-content/uploads/2020/11/25-films-enigme.jpg"
+        className="rounded-lg"
+      />
+    </div>
+  );
+};
